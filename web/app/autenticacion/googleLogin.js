f2a07ed2bf8b0140fcabfa88d764a4d3f403bbd2
luimstumbad00 <124089296+luimstumbad00@users.noreply.github.com>
2024-04-02 15:19:17 -0600

    ya funciona google

--- web/app/autenticacion/googleLogin.js
+++ web/app/autenticacion/googleLogin.js
@@ -1,24 +1,19 @@
 import { GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js"
 import { auth } from '../firebase.js'
 
-
 const googleButton = document.querySelector('#google-login')
 
 googleButton.addEventListener('click', async () => {
     const provider = new GoogleAuthProvider()
-    const email = null;
     try {
         const credentials = await signInWithPopup(auth, provider)
-        console.log(credentials)
-        email = credentials.user.email
+        const userEmail = credentials.user.email
         sessionStorage.setItem("userCredentials", JSON.stringify(credentials));
-        sessionStorage.setItem("email", email);
+        sessionStorage.setItem("email", userEmail);
         window.location.href = './home.html';
-        alert('Bienvenido ' + credentials.user.email + ' pulsa aceptar para continuar')
-
+        alert('Bienvenido ' + userEmail + '. Pulsa aceptar para continuar.')
     } catch (error) {
-        console.log(error)
-        
+        console.error('Error al iniciar sesión:', error.message);
+        alert('Error al iniciar sesión: ' + error.message);
     }
-
 })
